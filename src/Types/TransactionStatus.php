932899fed289ce648a46d449c406f4ca11c8431e
Alexis petit <alexis.petit@paygreen.fr>
2022-03-03 15:09:15 +0000

    Feature/ update status for trd

--- src/Types/TransactionStatus.php
+++ src/Types/TransactionStatus.php
@@ -11,6 +11,7 @@ class TransactionStatus
     public const STATUS_REFUSED = "REFUSED";
     public const STATUS_CANCELLED = "CANCELLED";
     public const STATUS_PENDING = "PENDING";
+    public const STATUS_WAITING = "WAITING";
     public const STATUS_REFUNDED = "REFUNDED";
     public const STATUS_EXPIRED = "EXPIRED";
 }
